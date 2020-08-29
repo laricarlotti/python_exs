f3e655a220bea5a065b697a13a5d453348d05577
LariCarlotti <larissa.carlotti@gmail.com>
2020-08-29 19:43:03 -0300

    Exercise 07: Math module

---
+++ ex_007.py
@@ -0,0 +1,11 @@
+# Exercise 07
+
+import math
+
+num = int(input("Type a new number: "))
+
+double = num * 2
+triple = num * 3
+square = math.sqrt(num)
+
+print(f"The number {num} double is {double}, it's triple is {triple}, and it's square root is {square}")
