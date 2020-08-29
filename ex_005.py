e1a9a76f6910c3ca51bf46bbfbce9018193c319b
LariCarlotti <larissa.carlotti@gmail.com>
2020-08-29 19:39:37 -0300

    Exercise 05: Arithmetic operators

---
+++ ex_005.py
@@ -0,0 +1,13 @@
+# Exercise 05
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+s = num1 + num2
+p = num1 * num2
+d = num1 / num2
+sub = num1 - num2
+di = num1 // num2
+sqr = num1 ** num2
+
+print(f"The sum is {s}, \n the product is {p}, \n the division is {d}, \n the subtraction is {sub}, \n the full division is {di} \n and the square is {sqr}")
