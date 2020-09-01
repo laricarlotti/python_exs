051497fdbece75c0fd2eda6bd78ce5664551256f
LariCarlotti <larissa.carlotti@gmail.com>
2020-09-01 09:32:09 -0300

    Exercise 11: Currency conversion

---
+++ ex_011.py
@@ -0,0 +1,16 @@
+# Exercise 11
+
+from time import sleep
+
+bufunfa = float(input("How much money do you want to convert? "))
+dollar = bufunfa * 5
+print(f"Conversion of R${bufunfa} \n")
+
+print("Converting...")
+print("In 3")
+sleep(1)
+print("2")
+sleep(1)
+print("1")
+sleep(1)
+print(f"You have US${dollar} of cash")
